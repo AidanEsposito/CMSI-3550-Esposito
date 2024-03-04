c61bb868c6caa867655392735aa50a4bce0398cd
AidanEsposito <97552458+AidanEsposito@users.noreply.github.com>
2024-03-03 16:14:13 -0800

    Updated Calculator

--- subnet_calculator/main.rs
+++ subnet_calculator/main.rs
@@ -1,36 +1,40 @@
 //Aidan Esposito
 
-use std::net::Ipv4Addr;
+use std::env::args;
 
-fn main() {
-    // Find target IP and Subnet Notation
-    let target_ip: Ipv4Addr = "157.242.0.0".parse().unwrap();
-    let cidr_notation = 16;
+fn main(){
+    for (i, arg) in args().enumerate(){
+        println!("Arg {i}:, {arg}");
 
-    // Subnet mask
-    let subnet_mask: u32 = 0xffffffff << (32 - cidr_notation);
-    let subnet_mask = Ipv4Addr::from(subnet_mask.to_be());
-
-    // Subnet values
-    let subnet_ip = target_ip;
-    let subnet_broadcast_ip = subnet_ip | !subnet_mask;
-    let subnet_host_min = u32::from(subnet_ip) + 1;
-    let subnet_host_max = u32::from(subnet_broadcast_ip) - 1;
-
-    // Next Network
-    let next_network_ip = u32::from(subnet_broadcast_ip) + 1;
-
-    // IP Address #
-    let num_addresses = 2u32.pow(32 - cidr_notation);
-
-    // Output subnet values
-    println!("Network ID: {}", subnet_ip);
-    println!("Broadcast IP: {}", subnet_broadcast_ip);
-    println!("First Host: {}", Ipv4Addr::from(subnet_host_min.to_be()));
-    println!("Last Host: {}", Ipv4Addr::from(subnet_host_max.to_be()));
-    println!("Next Network: {}", Ipv4Addr::from(next_network_ip.to_be()));
-    println!("Number of IP addresses: {}", num_addresses);
-    println!("CIDR/Subnet Mask: /{}", cidr_notation);
+        if i == 1{
+            calculate_subnet(arg);
+        }
+    }
 }
+
+fn calculate_subnet(target_subnet: String){
+    let parts = target_subnet.split("/");
+    for parts in parts{
+        println!("Subnet part: {part}");
+    }
+
+    let [target_ip, cidr]: [&str; 2] = target_subnet.split("/").collect::<Vec<&str>>().try_into().unwrap_or_default();
+
+    println!("Target IP: {target_ip}");
+    println!("CIDR: {cidr}");
+
+    let octet_strings: [&str; 4] = target_ip.split(".").collect::<Vec<&str>>().try_into().unwrap_or_default();
+
+    for octet in octet_strings{
+        let my_octet: i32 = octet.parse().unwrap();
+
+        let octet_math = my_octet + 1;
+        println!("{octet_math}")
+    }
+
+    //do subnet calculator from here!!!!
+
+}
+
 
 
